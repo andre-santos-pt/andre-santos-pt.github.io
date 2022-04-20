89ed41e30998030cd7bcc8b9d5f19863f5c91886
André L. Santos <andre.santos@iscte.pt>
2022-04-20 16:21:29 +0100

    Update windowswing.kt

--- kotlin/metaprogramacao/code/windowswing.kt
+++ kotlin/metaprogramacao/code/windowswing.kt
@@ -4,35 +4,35 @@ fun main() {
     frame.size = Dimension(500, 300)
     frame.layout = GridLayout(0, 2)
     frame.title = "dummy window"
-    val b1 = JButton("say hello")
-    b1.addActionListener {
+    val sayHelloButton = JButton("say hello")
+    sayHelloButton.addActionListener {
         JOptionPane.showMessageDialog(null, "hello!");
     }
-    frame.add(b1)
+    frame.add(sayHelloButton)
 
     val panel = JPanel()
     panel.layout = GridLayout(0, 1)
     val l = JLabel("?")
-    val b3 = JButton("show size")
-    b3.addActionListener {
+    val showSizeButton = JButton("show size")
+    showSizeButton.addActionListener {
         l.text = "${frame.size.width} x ${frame.size.height}"
     }
-    panel.add(b3)
+    panel.add(showSizeButton)
     panel.add(l)
     frame.add(panel)
 
     val panel2 = JPanel()
     panel2.layout = GridLayout(0, 2)
-    val moveRight = JButton("increase")
-    moveRight.addActionListener {
+    val increaseButton = JButton("increase")
+    increaseButton.addActionListener {
         frame.size = Dimension(frame.size.width*2, frame.size.height*2)
     }
-    panel2.add(moveRight)
-    val moveDown = JButton("decrease")
-    moveDown.addActionListener {
+    panel2.add(increaseButton)
+    val decreaseButton = JButton("decrease")
+    decreaseButton.addActionListener {
         frame.size = Dimension(frame.size.width/2, frame.size.height/2)
     }
-    panel2.add(moveDown)
+    panel2.add(decreaseButton)
     panel.add(panel2)
     frame.isVisible = true
 }
