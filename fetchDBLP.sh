7d14b49b6c0227ca3b5c0ff68e8a60dde840747d
André L. Santos <andre.santos@iscte.pt>
2026-05-19 16:45:15 +0100

    extra pubs

--- fetchDBLP.sh
+++ fetchDBLP.sh
@@ -2,5 +2,7 @@
 
 dblpurl='https://dblp.org/search/publ/api?q=author%3AAndré_L._Santos_0001%3A&format=json&h=100'
 curl $dblpurl | jq '.result.hits.hit' > pubsfetch.json
-jq 'del(.[] | select(.info.venue == "CoRR"))' pubsfetch.json > _data/pubs.json
+jq -s 'add | sort_by(.info.year | tonumber) | reverse' pubsfetch.json _data/extrapubs.json  > merge.json
+jq 'del(.[] | select(.info.venue == "CoRR"))' merge.json > _data/pubs.json
 rm pubsfetch.json
+rm merge.json
